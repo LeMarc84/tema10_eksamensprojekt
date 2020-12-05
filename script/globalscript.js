3a4af5bf6027d4aa074fcf33b0773b354d20b8fe
mort108v <61751696+mort108v@users.noreply.github.com>
2020-12-05 17:27:26 +0100

    Site untill now

---
+++ script/globalscript.js
@@ -0,0 +1,89 @@
+// COMPANYNAME ON LOAD
+const phonename = document.querySelector(".phonename");
+
+window.addEventListener('DOMContentLoaded', (slideLogo) => {
+    console.log('DOM loaded');
+
+    phonename.style.animation = `phonenameMove 0.5s ease forwards ${.6}s`;
+    console.log('phonenameMove');
+
+});
+
+//SPLASH IMAGE CHANGE
+
+var images = [], x = 0;
+images[0] = "../assets/splash/outside_light_gradient.png";
+images[1] = "../assets/splash/outside_light.png";
+images[2] = "../assets/splash/outside_dark_side.jpg";
+images[3] = "../assets/splash/karen-lolk-05zfcymXeD6yFq2MnebUcw.jfif";
+setInterval("changeImage()", 10000);
+var x = 0;
+
+changeImage();
+
+function changeImage() {
+    var img = document.getElementById("splashsrc");
+    img.src = images[x];
+    x++;
+    if(x >= images.length) {
+        x = 0;
+    }
+    fadeImg(img, 100, true);
+    setTimeout("changeImage()", 10000);
+}
+
+function fadeImg(el, val, fade) {
+    if(fade === true) {
+        val--;
+    } else {
+        val ++;
+    }
+    if(val > 0 && val < 100) {
+        el.style.opacity = val / 100;
+        setTimeout(function(){ fadeImg(el, val, fade); }, 100);
+    }
+}
+
+const lang = document.querySelector("#language");
+
+lang.addEventListener("click", () => {
+    console.log("Clicked flag");
+
+    const langDK = document.querySelector(".langDK");
+    const langUK = document.querySelector(".langUK");
+
+    if (langDK.classList.contains('block')) {
+
+        langDK.classList.toggle('block');
+        langUK.classList.toggle('none');
+
+    } else {
+        langDK.classList.toggle('none');
+        langUK.classList.toggle('block');
+    }
+})
+
+//NAVIGATION STARTER HER
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+    //Toggle Nav
+    nav.classList.toggle('nav-active');
+
+    //Animate Links (forEach går i gennem hvert link/Element)
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            //(hvis der allerede er en animation så fjernes den)
+            link.style.animation = ''
+        } else {
+            //(hvis der ikke er en animation, så tilføjes den
+            link.style.animation = `navLinkFade 0.5s ease forwards ${(index / 15)+.3}s`;
+        }
+    });
+    //Burger Animation (her fjernes eller påsættes toggle-klassen fra css)
+    burger.classList.toggle('toggle');
+});
+
+//NAVIGATION SLUTTER HER
---
+++ script/looppop.js
@@ -0,0 +1,72 @@
+  let json;
+  let valgt;
+  let element;
+
+  const popup = document.querySelector("#popup");
+  document.addEventListener("DOMContentLoaded", loadJSON);
+
+  let temp = document.querySelector("template");
+
+  let filter = "alle";
+  const link = "https://www.selya.dk/kea/10_eksamensprojekt/wordpress/wp-json/wp/v2/produkter";
+
+  async function loadJSON() {
+      const respons = await fetch(link);
+      json = await respons.json();
+      addEventListenerToButtons();
+      vis();
+  }
+
+  function vis() {
+
+      const templatePointer = document.querySelector("template");
+      let listPointer = document.querySelector(".list");
+
+      listPointer.innerHTML = " ";
+
+      //løb igennem array "element"
+      json.forEach(element => {
+          if (filter == "alle" || filter == element.kategori[0]) {
+              console.log(filter);
+
+              const klon = temp.cloneNode(true).content;
+              klon.querySelector("h2").textContent = element.overskrift;
+              klon.querySelector(".pris").textContent = element.pris + " kr.";
+              klon.querySelector("img").src = element.image.guid;
+
+              klon.querySelector("article").addEventListener("click", () => visDetaljer(element));
+
+              listPointer.appendChild(klon);
+          }
+      })
+  }
+
+  document.querySelector("#luk").addEventListener("click", () => popup.style.display = "none");
+
+  function visDetaljer(element) {
+      console.log(element);
+
+      popup.style.display = "block";
+      popup.querySelector("h2").textContent = element.overskrift;
+      popup.querySelector(".pris").textContent = element.pris + " kr.";
+      popup.querySelector("img").src = element.image.guid;
+      popup.querySelector(".beskrivelse").innerHTML = element.beskrivelse;
+  }
+
+  function addEventListenerToButtons() {
+      document.querySelectorAll(".filter").forEach((btn) => {
+          btn.addEventListener("click", filterBTNs);
+      });
+  }
+
+  function filterBTNs() {
+      filter = this.dataset.element;
+      document.querySelector("kol_h1").textContent = this.innerHTML;
+      document.querySelectorAll(".filter").forEach((btn) => {
+          btn.classList.remove("valgt");
+      })
+      this.classList.add("valgt");
+      vis();
+  }
+
+  loadJSON();
